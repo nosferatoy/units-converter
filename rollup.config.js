d0999df73ecbce7a4c82ef8d84fdefc1fa7c636d
nosferatoy <giorgospapa86@gmail.com>
2019-06-22 13:55:14 +0300

    add standard linter

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,39 @@
-import pkg from './package.json';
-import path from "path";
+import pkg from './package.json'
+import path from 'path'
 
 const digital = path.resolve(__dirname, 'src/units/digital.js')
 const volume = path.resolve(__dirname, 'src/units/volume.js')
 const utils = path.resolve(__dirname, 'src/utils.js')
 
-
 export default [
-	{
-		input: 'src/units/digital.js',
-		output: [
-			{ name: 'digital', file: 'dist/units/digital.js', format: 'es' },
-			// { name: 'digital', file: 'dist/units/digital.cjs.js', format: 'cjs' },
-		],
-		external: [utils]
-	},
-	{
-		input: 'src/units/volume.js',
-		output: [
-			{ name: 'volume', file: 'dist/units/volume.js', format: 'es' },
-			// { name: 'volume', file: 'dist/units/volume.cjs.js', format: 'cjs' },
-		],
-		external: [utils]
-	},
-	{
-		input: 'src/utils.js',
-		output: [
-			{ name: 'utils', file: 'dist/utils.js', format: 'es' },
-			// { name: 'utils', file: 'dist/utils.cjs.js', format: 'cjs' },
-		],
-	},
-	{
-		input: 'src/index.js',
-		output: [
-			// { file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
-		external: [digital, volume]
-	}]
+  {
+    input: 'src/units/digital.js',
+    output: [
+      { name: 'digital', file: 'dist/units/digital.js', format: 'es' }
+      // { name: 'digital', file: 'dist/units/digital.cjs.js', format: 'cjs' },
+    ],
+    external: [utils]
+  },
+  {
+    input: 'src/units/volume.js',
+    output: [
+      { name: 'volume', file: 'dist/units/volume.js', format: 'es' }
+      // { name: 'volume', file: 'dist/units/volume.cjs.js', format: 'cjs' },
+    ],
+    external: [utils]
+  },
+  {
+    input: 'src/utils.js',
+    output: [
+      { name: 'utils', file: 'dist/utils.js', format: 'es' }
+      // { name: 'utils', file: 'dist/utils.cjs.js', format: 'cjs' },
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      // { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    external: [digital, volume]
+  }]
